417d1f2a17c5861b1b4e28e32b7cb7efc2256357
IvanRusso91 <iv.russo91@gmail.com>
2022-06-08 18:22:32 +0200

    inserita icona di pagina

--- html.php
+++ html.php
@@ -81,6 +81,9 @@ $db=[
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
+  <!-- icona in cima alla pagina -->
+  <link rel="icon" href="img/icona.jpg">
+  
   <title>Domande frequenti - Privacy e termini - Google</title>
 </head>
 <body>
